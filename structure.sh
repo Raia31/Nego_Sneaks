1f241d5424bcc01651aae31cc5ab98b5c301f8d0
Dagale <144978155+Raia31@users.noreply.github.com>
2024-12-14 16:48:00 +0800

    initial commit

---
+++ structure.sh
@@ -0,0 +1,15 @@
+/project-folder
+├── index.html
+├── about-us.html
+├── contacts.html
+├── style.scss        <-- This is your SCSS file now.
+└── /images
+    ├── item1.jpg
+    ├── item2.jpg
+    ├── item3.jpg
+    ├── item4.jpg
+    ├── item5.jpg
+    ├── item6.jpg
+    ├── item7.jpg
+    ├── item8.jpg
+    └── item9.jpg
